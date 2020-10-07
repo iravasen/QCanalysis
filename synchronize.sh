3bfd562188e5039a539e042632d44b76debffda7
JianLIUhep <jian.liu@cern.ch>
2020-10-07 19:04:11 +0200

    added backup of logs

--- synchronize.sh
+++ synchronize.sh
@@ -10,6 +10,8 @@ do
 	rsync -avruh Plots/ShiftReport24h_FHR_* itsshift@lxplus.cern.ch:/eos/user/i/itsshift/Reports24h/FHR
 	rsync -avruh Data/Output_all-IB-layers_FHRMAPS* itsshift@lxplus.cern.ch:/eos/user/i/itsshift/Reports24h/Output/FHR
         rsync -avruh Data/Output_all-IB-layers_THRMAPS* itsshift@lxplus.cern.ch:/eos/user/i/itsshift/Reports24h/Output/THR
+	rsync -avruh logs/logTHR* itsshift@lxplus.cern.ch:/eos/user/i/itsshift/Reports24h/logs/THR
+	rsync -avruh logs/logFHR* itsshift@lxplus.cern.ch:/eos/user/i/itsshift/Reports24h/logs/FHR
 	echo "Sleeping 1h"
 	sleep 1h
 done
