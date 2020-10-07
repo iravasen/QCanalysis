dc9f9647884e77f3932a9814a6ca188591e9d600
JianLIUhep <jian.liu@cern.ch>
2020-10-07 10:39:45 +0200

    automatic synchronization of 24H reports

--- synchronize.sh
+++ synchronize.sh
@@ -1,2 +1,14 @@
-rsync -avruh Plots/ShiftReport24h_THR_* itsshift@lxplus.cern.ch:/eos/user/i/itsshift/Reports24h/THR
-rsync -avruh Plots/ShiftReport24h_FHR_* itsshift@lxplus.cern.ch:/eos/user/i/itsshift/Reports24h/FHR
+#!/bin/bash
+# Read Password
+echo -n "Password for itsshift:" 
+read -s password
+
+while true
+do
+	echo -n "$password" | kinit itsshift
+	rsync -avruh Plots/ShiftReport24h_THR_* itsshift@lxplus.cern.ch:/eos/user/i/itsshift/Reports24h/THR
+	rsync -avruh Plots/ShiftReport24h_FHR_* itsshift@lxplus.cern.ch:/eos/user/i/itsshift/Reports24h/FHR
+	echo "Sleeping 1h"
+	sleep 1h
+done
+
