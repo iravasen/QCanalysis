c0bdb13c9293b21846a68fa0afbbeefb9d6b53a4
Chiara De Martin <chiara.de.martin@cern.ch>
2021-02-16 12:59:35 +0100

    adapt plots also for OB

--- analysismacros/AnalyzeLayerOccupancy.C
+++ analysismacros/AnalyzeLayerOccupancy.C
@@ -32,14 +32,18 @@ void AnalyzeLayerOccupancy(){
   cout<<endl;
 
   bool isIB;
+
   if(fpath.find("IB")!=string::npos){
     isIB = kTRUE;
   }
+  else if (fpath.find("OB")!=string::npos){
+    isIB = kFALSE;
+  }
   else{
     string layernum = fpath.substr(fpath.find("Layer")+5, 1);
-    if(stoi(layernum)>=0 && stoi(layernum)<=2) nchips = 9;
-    else if (stoi(layernum)==3 && stoi(layernum)==4) nchips = 54*2;
-    else nchips = 98*2;
+    if(stoi(layernum)>=0 && stoi(layernum)<=2) nchips = 9; 
+    else if (stoi(layernum)==3 || stoi(layernum)==4) nchips = 8; //number of HICs 
+    else nchips = 14; //number of HICs
     if(nchips==9) isIB=kTRUE;
     else isIB=kFALSE;
   }
@@ -78,14 +82,14 @@ void SetStyle(TGraph *h, Int_t col, Style_t mkr){
 //
 // Analyse data
 //
-void DoAnalysis(string filepath, const int nChips, string skipruns, bool isIB){
+void DoAnalysis(string filepath, int nChips, string skipruns, bool isIB){
 
   gStyle->SetOptStat(0000);
 
   std::vector<TH2*> hmaps;
   std::vector<string> timestamps, runnumbers, laynums;
   int nTimes=0, nRuns=1;
-  Int_t col[] = {TColor::GetColor("#ff3300"), TColor::GetColor("#ec6e0a"), TColor::GetColor("#daaa14"), TColor::GetColor("#c7e51e"), TColor::GetColor("#85dd69"), TColor::GetColor("#42d6b4"), TColor::GetColor("#00ceff"), TColor::GetColor("#009adf"), TColor::GetColor("#0067c0"), TColor::GetColor("#0033a1")};
+  Int_t col[] = {TColor::GetColor("#ff3300"), TColor::GetColor("#ec6e0a"), TColor::GetColor("#daaa14"), TColor::GetColor("#c7e51e"), TColor::GetColor("#85dd69"), TColor::GetColor("#42d6b4"), TColor::GetColor("#00ceff"), TColor::GetColor("#009adf"), TColor::GetColor("#0067c0"), TColor::GetColor("#0033a1"), TColor::GetColor("#708090"), TColor::GetColor("#9400D3")};
 
   //Read the file and the list of plots with entries
   TFile *infile=new TFile(filepath.c_str());
@@ -97,11 +101,11 @@ void DoAnalysis(string filepath, const int nChips, string skipruns, bool isIB){
   while((key = ((TKey*)next()))){
     obj = key->ReadObj();
     if ((strcmp(obj->IsA()->GetName(),"TProfile")!=0)
-         && (!obj->InheritsFrom("TH2"))
-	       && (!obj->InheritsFrom("TH1"))
-       ) {
-            cout<<"<W> Object "<<obj->GetName()<<" is not 1D or 2D histogram : will not be converted"<<endl;
-       }
+	&& (!obj->InheritsFrom("TH2"))
+	&& (!obj->InheritsFrom("TH1"))
+	) {
+      cout<<"<W> Object "<<obj->GetName()<<" is not 1D or 2D histogram : will not be converted"<<endl;
+    }
     string objname = (string)obj->GetName();
     if(objname.find("Stv")!=string::npos) break;
     h2 = (TH2*)obj;
@@ -124,10 +128,14 @@ void DoAnalysis(string filepath, const int nChips, string skipruns, bool isIB){
     else nRuns=1;
   }
 
-  const int nLayers = (int)hmaps.size()==nRuns ? 1 : stoi(laynums[laynums.size()-1])+1;
+  const int nLayersIB = (int)hmaps.size()==nRuns ? 1 : stoi(laynums[laynums.size()-1])+1;
+  const int nLayersOB = (int)hmaps.size()==nRuns ? 1 : stoi(laynums[laynums.size()-1])+1-3;
+  int nLayers;
+  if (isIB) nLayers = nLayersIB;
+  else nLayers=nLayersOB;
 
   //const int nRuns = (int)runnumbers.size() / nLayers;
-  //cout<<"Lay: "<<nLayers<<"  Runs: "<<nRuns<<endl;
+  //  cout<<"Lay: "<<nLayers<<"  Runs: "<<nRuns<<endl;
   TGraph *trend[nLayers][100];
   int ilayer=nLayers-1;
   for(int ihist=(int)hmaps.size()-1; ihist>=0; ihist--){
@@ -143,8 +151,14 @@ void DoAnalysis(string filepath, const int nChips, string skipruns, bool isIB){
   TH1F *hproj = new TH1F();
   string histname = hmaps[0]->GetName();
   int irun=0;
+
   for(int ihist=(int)hmaps.size()-1; ihist>=0; ihist--){// start from the last in order to have the runs from the oldest to the newest
-    for(int ibiny=1; ibiny<=hmaps[ihist]->GetNbinsY(); ibiny++){//loop on y bins (staves)
+    if (!isIB){ //nChips depend on the layer for the OB
+      if (stoi(laynums[ihist]) ==3 || stoi(laynums[ihist]) ==4)    nChips=8;
+      else if (stoi(laynums[ihist]) ==5 || stoi(laynums[ihist]) ==6)    nChips=14;
+    }
+
+    for(int ibiny=1; ibiny<=hmaps[ihist]->GetNbinsY(); ibiny++){//loop on y bins (stave)s
       TH1D *hproj = hmaps[ihist]->ProjectionX("proj",ibiny,ibiny); //single stave
       trend[ilayer][ibiny-1]->SetName(Form("gr_L%s_stave%d",laynums[ihist].c_str(),ibiny-1));
       int deadchips = 0;
@@ -152,18 +166,51 @@ void DoAnalysis(string filepath, const int nChips, string skipruns, bool isIB){
         if(hmaps[ihist]->GetBinContent(ibinx,ibiny)<1e-15)
           deadchips++;
       }
-      if(deadchips>0)
-        cout<<"Layer "<<laynums[ihist]<<" Stave "<<ibiny-1<<" Run: "<<runnumbers[ihist]<<" --> Chips active:"<<nChips-deadchips<<endl;
-
+      if(deadchips>0){
+	if (isIB) {
+	  cout<<"\nLayer "<<laynums[ihist]<<" Stave "<<ibiny-1<<" Run: "<<runnumbers[ihist]<<" Chips number: " << nChips<<" --> Chips active:"<<nChips-deadchips<<endl;
+	} else {
+	  cout<<"\nLayer "<<laynums[ihist]<<" Stave "<<ibiny-1<<" Run: "<<runnumbers[ihist]<<" Hic number: " << nChips << " --> HICs active:"<<nChips-deadchips<<endl;
+	}
+      }
       if(deadchips!=nChips)
         trend[ilayer][ibiny-1]->SetPoint(irun, irun, hproj->Integral()/(nChips-deadchips));
       else
         trend[ilayer][ibiny-1]->SetPoint(irun, irun, 0.);
 
-      if((ibiny-1)<hmaps[ihist]->GetNbinsY()/2)
-        SetStyle(trend[ilayer][ibiny-1], col[ibiny-1], 24);
-      else
-        SetStyle(trend[ilayer][ibiny-1], col[ibiny-1-hmaps[ihist]->GetNbinsY()/2], 26);
+      double x=0;
+      double y=0;
+
+      if(isIB){
+	if((ibiny-1)<hmaps[ihist]->GetNbinsY()/2)
+	  SetStyle(trend[ilayer][ibiny-1], col[ibiny-1], 24);
+	else
+	  SetStyle(trend[ilayer][ibiny-1], col[ibiny-1-hmaps[ihist]->GetNbinsY()/2], 26);
+      }
+      else{
+	if (stoi(laynums[ihist]) ==3 || stoi(laynums[ihist])==4){
+	  if((ibiny-1)<hmaps[ihist]->GetNbinsY()/3){
+	    SetStyle(trend[ilayer][ibiny-1], col[ibiny-1], 24);
+	  }
+	  else if ((ibiny-1)<hmaps[ihist]->GetNbinsY()*2/3){
+	    SetStyle(trend[ilayer][ibiny-1], col[ibiny-1-hmaps[ihist]->GetNbinsY()/3], 26);
+	  }
+	  else{
+	    SetStyle(trend[ilayer][ibiny-1], col[ibiny-1-hmaps[ihist]->GetNbinsY()*2/3], 25);	    
+	  }
+	}
+	else if (stoi(laynums[ihist]) ==5 || stoi(laynums[ihist])==6){
+	  if((ibiny-1)<int(hmaps[ihist]->GetNbinsY()/4))
+	    SetStyle(trend[ilayer][ibiny-1], col[ibiny-1], 24);
+	  else if ((ibiny-1)<2*int(hmaps[ihist]->GetNbinsY()/4))
+	    SetStyle(trend[ilayer][ibiny-1], col[ibiny-1-int(hmaps[ihist]->GetNbinsY()/4)], 26);
+	  else if ((ibiny-1)<3*hmaps[ihist]->GetNbinsY()/4)
+	    SetStyle(trend[ilayer][ibiny-1], col[ibiny-1-2*int(hmaps[ihist]->GetNbinsY()/4)], 25);
+	  else
+	    SetStyle(trend[ilayer][ibiny-1], col[ibiny-1-int(hmaps[ihist]->GetNbinsY()*3/4)], 30);
+	}
+
+      }
     }
     irun++;
     if(ihist>0)
@@ -177,7 +224,7 @@ void DoAnalysis(string filepath, const int nChips, string skipruns, bool isIB){
   TH1F *hfake = new TH1F("hfake", "; Run; Fake-hit Rate (/event/pixel)", npoints, -0.5, (double)npoints-0.5);
 
   for(int ir=0; ir<(int)runnumbers.size()/nLayers; ir++)
-      hfake->GetXaxis()->SetBinLabel(ir+1, Form("run%06d", stoi(runnumbers[(int)runnumbers.size()/nLayers-1-ir])));
+    hfake->GetXaxis()->SetBinLabel(ir+1, Form("run%06d", stoi(runnumbers[(int)runnumbers.size()/nLayers-1-ir])));
 
 
   //Draw
@@ -187,8 +234,13 @@ void DoAnalysis(string filepath, const int nChips, string skipruns, bool isIB){
     canvas->SetLogy();
     canvas->SetTickx();
     canvas->SetTicky();
-    canvas->SetMargin(0.0988,0.1,0.194,0.0993);
-    TLegend *leg = new TLegend(0.904, 0.197,0.997,0.898);
+    canvas->SetMargin(0.0988,0.15,0.194,0.0993);
+    TLegend *leg = new TLegend(0.857, 0.197,0.997,0.898);
+
+    //setting a two column legend for L5 and L6
+    if (!isIB && nLayers==4 && (ilay==1 || ilay==2 || ilay==3)) leg->SetNColumns(2);
+    else  if (stoi(laynums[0]) ==4 || stoi(laynums[0]) ==5 || stoi(laynums[0]) ==6) leg->SetNColumns(2);
+    
     for(int istave=0; istave<hmaps[ilay*nRuns]->GetNbinsY(); istave++)
       leg->AddEntry(trend[ilay][istave], Form("Stv%d",istave), "p");
     hfake->GetYaxis()->SetRangeUser(1e-14, 1e-3);
