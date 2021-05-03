2b4b99465f00c16ac2998db74b52e8022de4209a
Chiara De Martin <chiara.de.martin@cern.ch>
2021-05-03 14:05:01 +0200

    Adaptation to OB + choose if hot maps should be saved or not

--- analysismacros/inc/constants.h
+++ analysismacros/inc/constants.h
@@ -1,6 +1,6 @@
 #include <TColor.h>
 //Constants
 const int nMasked = 5000;// max number of masked (hot) pixels
-const int nStavesInLay[3] = {12, 16, 20}; // only IB. FIX
+const int nStavesInLayAll[7] = {12, 16, 20, 24, 30, 42, 48}; 
 //colors
-Int_t col[] = {TColor::GetColor("#ff3300"), TColor::GetColor("#ec6e0a"), TColor::GetColor("#daaa14"), TColor::GetColor("#c7e51e"), TColor::GetColor("#85dd69"), TColor::GetColor("#42d6b4"), TColor::GetColor("#00ceff"), TColor::GetColor("#009adf"), TColor::GetColor("#0067c0"), TColor::GetColor("#0033a1")};
+Int_t col[] = {TColor::GetColor("#ff3300"), TColor::GetColor("#ec6e0a"), TColor::GetColor("#daaa14"), TColor::GetColor("#c7e51e"), TColor::GetColor("#85dd69"), TColor::GetColor("#42d6b4"), TColor::GetColor("#00ceff"), TColor::GetColor("#009adf"), TColor::GetColor("#0067c0"), TColor::GetColor("#0033a1"), TColor::GetColor("#708090"), TColor::GetColor("#9400D3")};
