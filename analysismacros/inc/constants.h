4a83b0ecac6371f36bf4d9c88bc930b49386114e
Chiara De Martin <chiara.de.martin@cern.ch>
2021-10-01 17:36:09 +0200

    change of color scale

--- analysismacros/inc/constants.h
+++ analysismacros/inc/constants.h
@@ -3,4 +3,5 @@
 const int nMasked = 5000;// max number of masked (hot) pixels
 const int nStavesInLayAll[7] = {12, 16, 20, 24, 30, 42, 48}; 
 //colors
-Int_t col[] = {TColor::GetColor("#ff3300"), TColor::GetColor("#ec6e0a"), TColor::GetColor("#daaa14"), TColor::GetColor("#c7e51e"), TColor::GetColor("#85dd69"), TColor::GetColor("#42d6b4"), TColor::GetColor("#00ceff"), TColor::GetColor("#009adf"), TColor::GetColor("#0067c0"), TColor::GetColor("#0033a1"), TColor::GetColor("#708090"), TColor::GetColor("#9400D3")};
+//Int_t col[] = {TColor::GetColor("#ff3300"), TColor::GetColor("#ec6e0a"), TColor::GetColor("#daaa14"), TColor::GetColor("#c7e51e"), TColor::GetColor("#85dd69"), TColor::GetColor("#42d6b4"), TColor::GetColor("#00ceff"), TColor::GetColor("#009adf"), TColor::GetColor("#0067c0"), TColor::GetColor("#0033a1"), TColor::GetColor("#708090"), TColor::GetColor("#9400D3")};
+Int_t col[] = {810, 807, 797, 827, 417, 841, 868, 867, 860, 602, 921, 874};
