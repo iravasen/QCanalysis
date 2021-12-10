afab9b833bc70b5c04fe023bafb124261efa0d13
agriselt <95351038+agriselt@users.noreply.github.com>
2021-12-10 13:24:57 +0100

    Add files via upload

--- analysismacros/trend_maker.C
+++ analysismacros/trend_maker.C
@@ -1,6 +1,6 @@
 
 void trend_maker(){
-
+gROOT->SetBatch(kTRUE);
 string fpath;
 cout<<"Available file(s) for the analysis of track task:"<<endl;
 gSystem->Exec("ls ../Data/*TrackTask* -Art | tail -n 500");
@@ -15,7 +15,7 @@ TFile *file = TFile::Open(fpath.c_str());
    TIter next(keyslist);
    TKey *key;
    vector<string>runs_clusage, runs_eta, runs_phi, runs_ncl, runs_occ;
-   vector<double>fcluster, etacounts, phicounts, phicounts1, phicounts2, ncluster, occ;
+   vector<double>fcluster, etacounts, relative_eta, phicounts, phicounts1, phicounts2, relative_phi1, relative_phi2, ncluster, rms_ncl, first_bin_edge_ncl, last_bin_edge_ncl, occ;
    vector<double>cycle_clusage, cycle_eta, cycle_phi, cycle_ncl, cycle_occ;
    double i,j,k,m,n=0;
 
@@ -85,7 +85,10 @@ TFile *file = TFile::Open(fpath.c_str());
 
           //Extract the mean value in x axis
           double x_mean = h->GetMean(1);
-
+          
+          //Integral of the distribution 
+          double integral = h->Integral();
+          
           //Find the bin number for a value in x axis
           TAxis* xaxis = h->GetXaxis();
           int binx = xaxis->FindBin(x_mean);
@@ -94,6 +97,15 @@ TFile *file = TFile::Open(fpath.c_str());
           double bin = h->GetBinContent(binx);
           //std::cout<<"etaCounts_y_value= "<< bin << std::endl;
           etacounts.push_back(bin);
+          
+          //Relative values of eta counts
+          double relative;
+          if (integral != 0){
+            relative = (bin/integral);
+          }
+          else {relative = 0;}
+          
+          relative_eta.push_back(relative);
      }
 
 //////////////////////////////////////////////////////////////////////////////////////////////
@@ -108,6 +120,10 @@ TFile *file = TFile::Open(fpath.c_str());
           cycle_phi.push_back(k);
 
           TH1D *h= (TH1D *) key->ReadObj();
+          
+          //Integral in range 0-2pi
+          double integral = h->Integral();
+                    
           //Extract the mean value in x axis
           double x_mean = h->GetMean(1);
           //Find the bin number for a value in x axis
@@ -120,6 +136,10 @@ TFile *file = TFile::Open(fpath.c_str());
 
           TH1D *hc1 = (TH1D *)h->Clone();
           hc1->GetXaxis()->SetRangeUser(0, 3);
+          
+          //Integral in range 0-pi
+          double integral1 = hc1->Integral();
+          
           //Extract the mean value in x axis
           double x_mean1 = hc1->GetMean(1);
           //Find the bin number for a value in x axis
@@ -132,6 +152,10 @@ TFile *file = TFile::Open(fpath.c_str());
 
           TH1D *hc2 = (TH1D *)h->Clone();
           hc2->GetXaxis()->SetRangeUser(3, 6);
+          
+          //Integral in range pi-2pi
+          double integral2 = hc2->Integral();
+          
           //Extract the mean value in x axis
           double x_mean2 = hc2->GetMean(1);
           //Find the bin number for a value in x axis
@@ -141,7 +165,23 @@ TFile *file = TFile::Open(fpath.c_str());
           double bin2 = hc2->GetBinContent(binx2);
           //cout<<"y value clone 2= "<<bin2<<endl;
           phicounts2.push_back(bin2);
-
+          
+          //Relative values
+          double rel_phi1;
+          if (integral != 0){
+            rel_phi1 = (integral1/integral);
+          }
+          else {rel_phi1 = 0;}
+          
+          relative_phi1.push_back(rel_phi1);
+          
+          double rel_phi2;
+          if (integral != 0){
+            rel_phi2 = (integral2/integral);
+          }
+          else {rel_phi2 = 0;}
+          
+          relative_phi2.push_back(rel_phi2);
       }
 
 //////////////////////////////////////////////////////////////////////////////////////////////
@@ -155,11 +195,36 @@ TFile *file = TFile::Open(fpath.c_str());
           runs_ncl.push_back(runnum);
           cycle_ncl.push_back(m);
 
-          TH1D *h= (TH1D *) key->ReadObj();
+          TH1D *h= (TH1D *) key->ReadObj();         
 
          //Extract the mean value in x axis
          double x_mean = h->GetMean(1);
          ncluster.push_back(x_mean);
+         
+         //Extract RMS of the distribution
+         double rms = h->GetRMS();
+         rms_ncl.push_back(rms);
+         
+         //bin low edge of the leftmost bin filled
+         double first_edge;
+         if ((h->FindFirstBinAbove())>0){
+           first_edge = h->GetXaxis()->GetBinLowEdge(h->FindFirstBinAbove());         
+         }
+         else{ 
+           first_edge =0;
+         }           
+         first_bin_edge_ncl.push_back(first_edge);       
+         
+         //bin low edge of the rightmost bin filled
+         double last_edge;
+         if ((h->FindLastBinAbove())>0){
+           last_edge = h->GetXaxis()->GetBinLowEdge(h->FindLastBinAbove());
+         }
+         else{
+           last_edge = 0;
+         }         
+         last_bin_edge_ncl.push_back(last_edge);
+
      }
 
 //////////////////////////////////////////////////////////////////////////////////////////////
@@ -194,12 +259,12 @@ TFile *file = TFile::Open(fpath.c_str());
   std::reverse(runs_clusage.begin(),runs_clusage.end());
   std::reverse(fcluster.begin(),fcluster.end());
 
-  auto c1 = new TCanvas();
+  auto c1 = new TCanvas("Fraction of clusters used in tracking");
   c1->SetGrid(0,1);
   auto gr = new TGraph(cycle_clusage.size(),&cycle_clusage[0],&fcluster[0]);
   double b=0;
   gr->GetXaxis()->SetNdivisions(cycle_clusage.size());
-  gr->GetXaxis()->Set(2+cycle_clusage.size(),gr->GetXaxis()->GetXmin(),2+cycle_clusage.size());
+  gr->GetXaxis()->Set(1+cycle_clusage.size(),gr->GetXaxis()->GetXmin(),1+cycle_clusage.size());
 
   for (auto itr : runs_clusage){
      b++;
@@ -208,7 +273,7 @@ TFile *file = TFile::Open(fpath.c_str());
       gr->GetXaxis()->ChangeLabel(binIndex,60,-1,39,-1,-1);
   }
   gr->GetXaxis()->SetLabelSize(0.027);
-  gr->SetTitle("Fraction of clusters used in tracking");
+  gr->SetNameTitle("Fraction of clusters used in tracking", "Fraction of clusters used in tracking");
   gr->GetXaxis()->SetTitle("Run");
   gr->GetYaxis()->SetTitle("nCluster in track/total cluster");
   gr->SetMarkerStyle(20);
@@ -217,14 +282,14 @@ TFile *file = TFile::Open(fpath.c_str());
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
   std::reverse(runs_eta.begin(),runs_eta.end());
-  std::reverse(etacounts.begin(),etacounts.end());
+  std::reverse(relative_eta.begin(),relative_eta.end());
 
-  auto c2 = new TCanvas();
+  auto c2 = new TCanvas("Relative number of tracks in Eta distributions");
   c2->SetGrid(0,1);
-  auto gr2 = new TGraph(cycle_eta.size(),&cycle_eta[0],&etacounts[0]);
+  auto gr2 = new TGraph(cycle_eta.size(),&cycle_eta[0],&relative_eta[0]);
   double b2=0;
   gr2->GetXaxis()->SetNdivisions(cycle_eta.size());
-  gr2->GetXaxis()->Set(2+cycle_eta.size(),gr->GetXaxis()->GetXmin(),2+cycle_eta.size());
+  gr2->GetXaxis()->Set(1+cycle_eta.size(),gr->GetXaxis()->GetXmin(),1+cycle_eta.size());
 
   for (auto itr : runs_eta){
      b2++;
@@ -233,9 +298,9 @@ TFile *file = TFile::Open(fpath.c_str());
       gr2->GetXaxis()->ChangeLabel(binIndex,60,-1,39,-1,-1);
   }
   gr2->GetXaxis()->SetLabelSize(0.027);
-  gr2->SetTitle("Max. of tracks in Eta distributions");
+  gr2->SetNameTitle("Relative number of tracks in Eta distributions", "Relative number of tracks in Eta distributions");
   gr2->GetXaxis()->SetTitle("Run");
-  gr2->GetYaxis()->SetTitle("<counts>");
+  gr2->GetYaxis()->SetTitle("mean counts / total counts");
   gr2->SetMarkerStyle(20);
   gr2->Draw("APL");
 
@@ -243,32 +308,58 @@ TFile *file = TFile::Open(fpath.c_str());
 
   std::reverse(runs_phi.begin(),runs_phi.end());
   std::reverse(phicounts.begin(),phicounts.end());
-  std::reverse(phicounts1.begin(),phicounts1.end());
-  std::reverse(phicounts2.begin(),phicounts2.end());
 
-  auto c3 = new TCanvas();
+  auto c3 = new TCanvas("Mean number of tracks in Phi distributions (0 < Phi < 2#pi)");
   c3->SetGrid(0,1);
 
   auto gr3 = new TGraph(cycle_phi.size(),&cycle_phi[0],&phicounts[0]);
-  auto gr31 = new TGraph(cycle_phi.size(),&cycle_phi[0],&phicounts1[0]);
-  auto gr32 = new TGraph(cycle_phi.size(),&cycle_phi[0],&phicounts2[0]);
   double b3=0;
 
-  gr31->GetXaxis()->SetNdivisions(cycle_phi.size());
-  gr31->GetXaxis()->Set(2+cycle_phi.size(),gr->GetXaxis()->GetXmin(),2+cycle_phi.size());
+  gr3->GetXaxis()->SetNdivisions(cycle_phi.size());
+  gr3->GetXaxis()->Set(1+cycle_phi.size(),gr3->GetXaxis()->GetXmin(),1+cycle_phi.size());
 
   for (auto itr : runs_phi){
      b3++;
-     int binIndex=gr31->GetXaxis()->FindBin(b3);
+     int binIndex=gr3->GetXaxis()->FindBin(b3);
+      gr3->GetXaxis()->SetBinLabel(binIndex,itr.data());
+      gr3->GetXaxis()->ChangeLabel(binIndex,60,-1,39,-1,-1);
+  }
+
+  gr3->GetXaxis()->SetLabelSize(0.027);
+  gr3->SetNameTitle("Mean number of tracks in Phi distributions (0 < Phi < 2#pi)", "Mean number of tracks in Phi distributions (0 < Phi < 2#pi)");
+  gr3->GetXaxis()->SetTitle("Run");
+  gr3->GetYaxis()->SetTitle("<counts>");
+  gr3->SetMarkerStyle(20);
+  gr3->Draw("APL");
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  std::reverse(phicounts1.begin(),phicounts1.end());
+  std::reverse(phicounts2.begin(),phicounts2.end());
+  std::reverse(relative_phi1.begin(),relative_phi1.end());
+  std::reverse(relative_phi2.begin(),relative_phi2.end());
+  
+  auto c31 = new TCanvas("Relative number of tracks in Phi distributions");
+  c31->SetGrid(0,1);
+
+  auto gr31 = new TGraph(cycle_phi.size(),&cycle_phi[0],&relative_phi1[0]);
+  auto gr32 = new TGraph(cycle_phi.size(),&cycle_phi[0],&relative_phi2[0]);
+  double b31=0;
+
+  gr31->GetXaxis()->SetNdivisions(cycle_phi.size());
+  gr31->GetXaxis()->Set(1+cycle_phi.size(),gr31->GetXaxis()->GetXmin(),1+cycle_phi.size());
+
+  for (auto itr : runs_phi){
+     b31++;
+     int binIndex=gr31->GetXaxis()->FindBin(b31);
       gr31->GetXaxis()->SetBinLabel(binIndex,itr.data());
       gr31->GetXaxis()->ChangeLabel(binIndex,60,-1,39,-1,-1);
   }
 
   gr31->GetXaxis()->SetLabelSize(0.027);
-  gr31->SetTitle("Max. of tracks in Phi distributions");
+  gr31->SetNameTitle("Relative number of tracks in Phi distributions", "Relative number of tracks in Phi distributions");
   gr31->GetXaxis()->SetTitle("Run");
-  gr31->GetYaxis()->SetTitle("<counts>");
-  gr31->GetHistogram()->SetMinimum(0.); //Set minimum y value
+  gr31->GetYaxis()->SetTitle("Number of counts in subrange / Total counts 0-2#pi");
   gr31->SetMarkerStyle(20);
   gr31->SetMarkerColor(kRed);
   gr31->SetLineColor(kRed);
@@ -277,16 +368,12 @@ TFile *file = TFile::Open(fpath.c_str());
   gr32->SetMarkerColor(kBlue);
   gr32->SetLineColor(kBlue);
   gr32->Draw("PL SAME");
-  gr3->SetMarkerStyle(20);
-  gr3->SetMarkerColor(kBlack);
-  gr3->SetLineColor(kBlack);
-  gr3->Draw("PL SAME");
-  auto legend=new TLegend();
+
+  auto legend=new TLegend(0.769784,0.842456,0.91207,0.927515);
   legend->SetFillColor(0);
-  legend->SetHeader("Phi range","");
+  legend->SetHeader("Phi subrange","");
   legend->AddEntry(gr31,"0-#pi","lp");
   legend->AddEntry(gr32,"#pi-2#pi","lp");
-  legend->AddEntry(gr3,"0-2#pi","lp");
   legend->Draw();
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -294,12 +381,12 @@ TFile *file = TFile::Open(fpath.c_str());
   std::reverse(runs_ncl.begin(),runs_ncl.end());
   std::reverse(ncluster.begin(),ncluster.end());
 
-  auto c4 = new TCanvas();
+  auto c4 = new TCanvas("hNClusters");
   c4->SetGrid(0,1);
   auto gr4 = new TGraph(cycle_ncl.size(),&cycle_ncl[0],&ncluster[0]);
   double b4=0;
   gr4->GetXaxis()->SetNdivisions(cycle_ncl.size());
-  gr4->GetXaxis()->Set(2+cycle_ncl.size(),gr4->GetXaxis()->GetXmin(),2+cycle_ncl.size());
+  gr4->GetXaxis()->Set(1+cycle_ncl.size(),gr4->GetXaxis()->GetXmin(),1+cycle_ncl.size());
 
   for (auto itr : runs_ncl){
      b4++;
@@ -308,23 +395,88 @@ TFile *file = TFile::Open(fpath.c_str());
       gr4->GetXaxis()->ChangeLabel(binIndex,60,-1,39,-1,-1);
   }
   gr4->GetXaxis()->SetLabelSize(0.027);
-  gr4->SetTitle("hNClusters");
+  gr4->SetNameTitle("hNClusters", "hNClusters");
   gr4->GetXaxis()->SetTitle("Run");
   gr4->GetYaxis()->SetTitle("<Number of clusters per track>");
   gr4->SetMarkerStyle(20);
   gr4->Draw("APL");
+  
+//////////////////////////////////////////NClusters RMS//////////////////////////////////////////////////////////////////////////////////////////
+
+  std::reverse(rms_ncl.begin(),rms_ncl.end());
+
+  auto c41 = new TCanvas("RMS_NClustersDistribution");
+  c41->SetGrid(0,1);
+  auto gr41 = new TGraph(cycle_ncl.size(),&cycle_ncl[0],&rms_ncl[0]);
+  double b41=0;
+  gr41->GetXaxis()->SetNdivisions(cycle_ncl.size());
+  gr41->GetXaxis()->Set(1+cycle_ncl.size(),gr41->GetXaxis()->GetXmin(),1+cycle_ncl.size());
+
+  for (auto itr : runs_ncl){
+     b41++;
+     int binIndex=gr41->GetXaxis()->FindBin(b41);
+      gr41->GetXaxis()->SetBinLabel(binIndex,itr.data());
+      gr41->GetXaxis()->ChangeLabel(binIndex,60,-1,39,-1,-1);
+  }
+  gr41->GetXaxis()->SetLabelSize(0.027);
+  gr41->SetNameTitle("RMS_NClustersDistribution", "RMS_NClustersDistribution");
+  gr41->GetXaxis()->SetTitle("Run");
+  gr41->GetYaxis()->SetTitle("<RMS>");
+  gr41->SetMarkerStyle(20);
+  gr41->Draw("APL");
+  
+
+//////////////////////////////////////////NClusters. Bin low edge of the first and last bins filled//////////////////////////////////////
+
+  std::reverse(first_bin_edge_ncl.begin(),first_bin_edge_ncl.end());
+  std::reverse(last_bin_edge_ncl.begin(),last_bin_edge_ncl.end());
+  
+  auto c43 = new TCanvas("Low edges of first and last bins filled in NClustersDistribution");
+  c43->SetGrid(0,1);
+  auto gr43 = new TGraph(cycle_ncl.size(),&cycle_ncl[0],&last_bin_edge_ncl[0]);
+  auto gr42 = new TGraph(cycle_ncl.size(),&cycle_ncl[0],&first_bin_edge_ncl[0]);
+  double b43=0;
+  gr43->GetXaxis()->SetNdivisions(cycle_ncl.size());
+  gr43->GetXaxis()->Set(1+cycle_ncl.size(),gr43->GetXaxis()->GetXmin(),1+cycle_ncl.size());
+
+  for (auto itr : runs_ncl){
+     b43++;
+     int binIndex=gr43->GetXaxis()->FindBin(b43);
+      gr43->GetXaxis()->SetBinLabel(binIndex,itr.data());
+      gr43->GetXaxis()->ChangeLabel(binIndex,60,-1,39,-1,-1);
+  }
+  gr43->GetXaxis()->SetLabelSize(0.027);
+  gr43->SetNameTitle("Low edges of first and last bins filled in NClustersDistribution", "Low edges of first and last bins filled in NClustersDistribution");
+  gr43->GetXaxis()->SetTitle("Run");
+  gr43->GetYaxis()->SetTitle("Value in x axis");
+  gr43->SetMarkerStyle(20);
+  gr43->SetMarkerColor(kRed);
+  gr43->SetLineColor(kRed);
+  gr43->GetHistogram()->SetMinimum(0.); //Set minimum y value
+  gr43->Draw("APL");
+  gr42->SetMarkerStyle(20);
+  gr42->SetMarkerColor(kBlue);
+  gr42->SetLineColor(kBlue);
+  gr42->Draw("PL SAME"); 
+  auto legend1=new TLegend(0.809353,0.830621,0.951639,0.914941);
+  legend1->SetFillColor(0);
+  //legend1->SetHeader("Phi range","");
+  legend1->AddEntry(gr42,"leftmost bin filled","lp");
+  legend1->AddEntry(gr43,"rightmost bin filled","lp");
+  legend1->Draw();
+ 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
   std::reverse(runs_occ.begin(),runs_occ.end());
   std::reverse(occ.begin(),occ.end());
 
-  auto c5 = new TCanvas();
+  auto c5 = new TCanvas("Track occupancy in ROF");
   c5->SetGrid(0,1);
   auto gr5 = new TGraph(cycle_occ.size(),&cycle_occ[0],&occ[0]);
   double b5=0;
   gr5->GetXaxis()->SetNdivisions(cycle_occ.size());
-  gr5->GetXaxis()->Set(2+cycle_occ.size(),gr5->GetXaxis()->GetXmin(),2+cycle_occ.size());
+  gr5->GetXaxis()->Set(1+cycle_occ.size(),gr5->GetXaxis()->GetXmin(),1+cycle_occ.size());
 
   for (auto itr : runs_occ){
      b5++;
@@ -333,11 +485,23 @@ TFile *file = TFile::Open(fpath.c_str());
       gr5->GetXaxis()->ChangeLabel(binIndex,60,-1,39,-1,-1);
   }
   gr5->GetXaxis()->SetLabelSize(0.027);
-  gr5->SetTitle("Track occupancy in ROF");
+  gr5->SetNameTitle("Track occupancy in ROF", "Track occupancy in ROF");
   gr5->GetXaxis()->SetTitle("Run");
   gr5->GetYaxis()->SetTitle("nTracks/ROF");
   gr5->SetMarkerStyle(20);
   gr5->Draw("APL");
+  
+TFile *f = new TFile("../Plots/TrackTask_Result_trends.root", "RECREATE");
+f->cd();
+c1->Write();
+c2->Write();
+c3->Write();
+c31->Write();
+c4->Write();
+c41->Write();
+c43->Write();
+c5->Write();
+f->Close();
 
 }
 
