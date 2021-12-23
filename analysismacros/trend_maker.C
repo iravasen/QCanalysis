a73788954d6bdb78b78046ada38f3a9ee96f7198
agriselt <95351038+agriselt@users.noreply.github.com>
2021-12-23 12:19:36 +0100

    Add files via upload

--- analysismacros/trend_maker.C
+++ analysismacros/trend_maker.C
@@ -15,7 +15,7 @@ TFile *file = TFile::Open(fpath.c_str());
    TIter next(keyslist);
    TKey *key;
    vector<string>runs_clusage, runs_eta, runs_phi, runs_ncl, runs_occ;
-   vector<double>fcluster, etacounts, relative_eta, phicounts, phicounts1, phicounts2, relative_phi1, relative_phi2, ncluster, rms_ncl, first_bin_edge_ncl, last_bin_edge_ncl, occ;
+   vector<double>fcluster, eta_ave, etacounts, relative_eta1, relative_eta2, phi_ave, phicounts, phicounts1, phicounts2, relative_phi1, relative_phi2, ncluster, rms_ncl, first_bin_edge_ncl, last_bin_edge_ncl, occ;
    vector<double>cycle_clusage, cycle_eta, cycle_phi, cycle_ncl, cycle_occ;
    double i,j,k,m,n=0;
 
@@ -82,12 +82,14 @@ TFile *file = TFile::Open(fpath.c_str());
           cycle_eta.push_back(j);
 
           TH1D *h= (TH1D *) key->ReadObj();
+          
+          //Integral in all range
+          double integral = h->Integral();
 
           //Extract the mean value in x axis
           double x_mean = h->GetMean(1);
+          eta_ave.push_back(x_mean);
           
-          //Integral of the distribution 
-          double integral = h->Integral();
           
           //Find the bin number for a value in x axis
           TAxis* xaxis = h->GetXaxis();
@@ -98,14 +100,35 @@ TFile *file = TFile::Open(fpath.c_str());
           //std::cout<<"etaCounts_y_value= "<< bin << std::endl;
           etacounts.push_back(bin);
           
-          //Relative values of eta counts
-          double relative;
-          if (integral != 0){
-            relative = (bin/integral);
-          }
-          else {relative = 0;}
+          TH1D *hc1 = (TH1D *)h->Clone();
+          hc1->GetXaxis()->SetRangeUser(-1.5, 0);
           
-          relative_eta.push_back(relative);
+          //Integral in range (-pi/2)-(0)
+          double integral1 = hc1->Integral();
+          
+          TH1D *hc2 = (TH1D *)h->Clone();
+          hc2->GetXaxis()->SetRangeUser(0, 1.5);
+          
+          //Integral in range 0-pi/2
+          double integral2 = hc2->Integral();
+          
+          //Relative values
+          double rel_eta1;
+          if (integral != 0){
+            rel_eta1 = (integral1/integral);
+          }
+          else {rel_eta1 = 0;}
+          
+          relative_eta1.push_back(rel_eta1);
+          
+          double rel_eta2;
+          if (integral != 0){
+            rel_eta2 = (integral2/integral);
+          }
+          else {rel_eta2 = 0;}
+          
+          relative_eta2.push_back(rel_eta2);
+
      }
 
 //////////////////////////////////////////////////////////////////////////////////////////////
@@ -126,6 +149,7 @@ TFile *file = TFile::Open(fpath.c_str());
                     
           //Extract the mean value in x axis
           double x_mean = h->GetMean(1);
+          phi_ave.push_back(x_mean);
           //Find the bin number for a value in x axis
           TAxis* xaxis = h->GetXaxis();
           int binx = xaxis->FindBin(x_mean);
@@ -267,7 +291,7 @@ TFile *file = TFile::Open(fpath.c_str());
   gr->GetXaxis()->Set(1+cycle_clusage.size(),gr->GetXaxis()->GetXmin(),1+cycle_clusage.size());
 
   for (auto itr : runs_clusage){
-     b++;
+     ++b;
      int binIndex=gr->GetXaxis()->FindBin(b);
       gr->GetXaxis()->SetBinLabel(binIndex,itr.data());
       gr->GetXaxis()->ChangeLabel(binIndex,60,-1,39,-1,-1);
@@ -282,53 +306,103 @@ TFile *file = TFile::Open(fpath.c_str());
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
   std::reverse(runs_eta.begin(),runs_eta.end());
-  std::reverse(relative_eta.begin(),relative_eta.end());
+  std::reverse(eta_ave.begin(),eta_ave.end());
+ // std::reverse(relative_eta.begin(),relative_eta.end());
 
-  auto c2 = new TCanvas("Relative number of tracks in Eta distributions");
+  auto c2 = new TCanvas("Average of Eta distributions");
   c2->SetGrid(0,1);
-  auto gr2 = new TGraph(cycle_eta.size(),&cycle_eta[0],&relative_eta[0]);
+  auto gr2 = new TGraph(cycle_eta.size(),&cycle_eta[0],&eta_ave[0]);
   double b2=0;
   gr2->GetXaxis()->SetNdivisions(cycle_eta.size());
   gr2->GetXaxis()->Set(1+cycle_eta.size(),gr->GetXaxis()->GetXmin(),1+cycle_eta.size());
 
   for (auto itr : runs_eta){
-     b2++;
+     ++b2;
      int binIndex=gr2->GetXaxis()->FindBin(b2);
       gr2->GetXaxis()->SetBinLabel(binIndex,itr.data());
       gr2->GetXaxis()->ChangeLabel(binIndex,60,-1,39,-1,-1);
   }
   gr2->GetXaxis()->SetLabelSize(0.027);
-  gr2->SetNameTitle("Relative number of tracks in Eta distributions", "Relative number of tracks in Eta distributions");
+  gr2->SetNameTitle("Average of Eta distributions", "Average of Eta distributions");
   gr2->GetXaxis()->SetTitle("Run");
-  gr2->GetYaxis()->SetTitle("mean counts / total counts");
+  gr2->GetYaxis()->SetTitle("Mean angle");
   gr2->SetMarkerStyle(20);
   gr2->Draw("APL");
+  
+  
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  std::reverse(relative_eta1.begin(),relative_eta1.end());
+  std::reverse(relative_eta2.begin(),relative_eta2.end());
+  
+  auto c21 = new TCanvas("Relative number of tracks in Eta distributions");
+  c21->SetGrid(0,1);
+
+  auto gr21 = new TGraph(cycle_eta.size(),&cycle_eta[0],&relative_eta1[0]);
+  auto gr22 = new TGraph(cycle_eta.size(),&cycle_eta[0],&relative_eta2[0]);
+  double b21=0;
+
+  gr21->GetXaxis()->SetNdivisions(cycle_eta.size());
+  gr21->GetXaxis()->Set(1+cycle_eta.size(),gr21->GetXaxis()->GetXmin(),1+cycle_eta.size());
+
+  for (auto itr : runs_eta){
+     ++b21;
+     int binIndex=gr21->GetXaxis()->FindBin(b21);
+      gr21->GetXaxis()->SetBinLabel(binIndex,itr.data());
+      gr21->GetXaxis()->ChangeLabel(binIndex,60,-1,39,-1,-1);
+  }
+  
+  gr21->GetYaxis()->SetRangeUser(0,1.085);
+  gr21->GetXaxis()->SetLabelSize(0.027);
+  gr21->SetNameTitle("Relative number of tracks in Eta distributions", "Relative number of tracks in Eta distributions");
+  gr21->GetXaxis()->SetTitle("Run");
+  gr21->GetYaxis()->SetTitle("Number of counts in subrange / Total counts (-#pi/2)-(#pi/2)");
+  gr21->SetMarkerStyle(20);
+  gr21->SetMarkerColor(kRed);
+  gr21->SetLineColor(kRed);
+  gr21->Draw("APL");
+  gr22->SetMarkerStyle(20);
+  gr22->SetMarkerColor(kBlue);
+  gr22->SetLineColor(kBlue);
+  gr22->Draw("PL SAME");
+
+  auto legend=new TLegend(0.773381,0.85429,0.905276,0.909763);
+  legend->SetNColumns(2);
+  legend->SetFillColor(0);
+  legend->SetHeader("Eta subrange","");
+  legend->AddEntry(gr21,"-#pi/2-0","lp");
+  legend->AddEntry(gr22,"0-#pi/2","lp");
+  legend->Draw();
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
   std::reverse(runs_phi.begin(),runs_phi.end());
   std::reverse(phicounts.begin(),phicounts.end());
+  std::reverse(phi_ave.begin(),phi_ave.end());
 
-  auto c3 = new TCanvas("Mean number of tracks in Phi distributions (0 < Phi < 2#pi)");
+  auto c3 = new TCanvas("Average of Phi distributions (0 < Phi < 2#pi)");
   c3->SetGrid(0,1);
 
-  auto gr3 = new TGraph(cycle_phi.size(),&cycle_phi[0],&phicounts[0]);
+  auto gr3 = new TGraph(cycle_phi.size(),&cycle_phi[0],&phi_ave[0]);
   double b3=0;
 
   gr3->GetXaxis()->SetNdivisions(cycle_phi.size());
   gr3->GetXaxis()->Set(1+cycle_phi.size(),gr3->GetXaxis()->GetXmin(),1+cycle_phi.size());
 
   for (auto itr : runs_phi){
-     b3++;
+     ++b3;
      int binIndex=gr3->GetXaxis()->FindBin(b3);
       gr3->GetXaxis()->SetBinLabel(binIndex,itr.data());
       gr3->GetXaxis()->ChangeLabel(binIndex,60,-1,39,-1,-1);
   }
 
   gr3->GetXaxis()->SetLabelSize(0.027);
-  gr3->SetNameTitle("Mean number of tracks in Phi distributions (0 < Phi < 2#pi)", "Mean number of tracks in Phi distributions (0 < Phi < 2#pi)");
+  gr3->SetNameTitle("Average of Phi distributions (0 < Phi < 2#pi)", "Average of Phi distributions (0 < Phi < 2#pi)");
   gr3->GetXaxis()->SetTitle("Run");
-  gr3->GetYaxis()->SetTitle("<counts>");
+  gr3->GetYaxis()->SetTitle("Mean angle");
   gr3->SetMarkerStyle(20);
   gr3->Draw("APL");
 
@@ -350,12 +424,13 @@ TFile *file = TFile::Open(fpath.c_str());
   gr31->GetXaxis()->Set(1+cycle_phi.size(),gr31->GetXaxis()->GetXmin(),1+cycle_phi.size());
 
   for (auto itr : runs_phi){
-     b31++;
+     ++b31;
      int binIndex=gr31->GetXaxis()->FindBin(b31);
       gr31->GetXaxis()->SetBinLabel(binIndex,itr.data());
       gr31->GetXaxis()->ChangeLabel(binIndex,60,-1,39,-1,-1);
   }
-
+  
+  gr31->GetYaxis()->SetRangeUser(0,1);
   gr31->GetXaxis()->SetLabelSize(0.027);
   gr31->SetNameTitle("Relative number of tracks in Phi distributions", "Relative number of tracks in Phi distributions");
   gr31->GetXaxis()->SetTitle("Run");
@@ -369,12 +444,13 @@ TFile *file = TFile::Open(fpath.c_str());
   gr32->SetLineColor(kBlue);
   gr32->Draw("PL SAME");
 
-  auto legend=new TLegend(0.769784,0.842456,0.91207,0.927515);
-  legend->SetFillColor(0);
-  legend->SetHeader("Phi subrange","");
-  legend->AddEntry(gr31,"0-#pi","lp");
-  legend->AddEntry(gr32,"#pi-2#pi","lp");
-  legend->Draw();
+  auto legend1=new TLegend(0.773381,0.85429,0.905276,0.909763);
+  legend1->SetNColumns(2);
+  legend1->SetFillColor(0);
+  legend1->SetHeader("Phi subrange","");
+  legend1->AddEntry(gr31,"0-#pi","lp");
+  legend1->AddEntry(gr32,"#pi-2#pi","lp");
+  legend1->Draw();
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -389,7 +465,7 @@ TFile *file = TFile::Open(fpath.c_str());
   gr4->GetXaxis()->Set(1+cycle_ncl.size(),gr4->GetXaxis()->GetXmin(),1+cycle_ncl.size());
 
   for (auto itr : runs_ncl){
-     b4++;
+     ++b4;
      int binIndex=gr4->GetXaxis()->FindBin(b4);
       gr4->GetXaxis()->SetBinLabel(binIndex,itr.data());
       gr4->GetXaxis()->ChangeLabel(binIndex,60,-1,39,-1,-1);
@@ -413,7 +489,7 @@ TFile *file = TFile::Open(fpath.c_str());
   gr41->GetXaxis()->Set(1+cycle_ncl.size(),gr41->GetXaxis()->GetXmin(),1+cycle_ncl.size());
 
   for (auto itr : runs_ncl){
-     b41++;
+     ++b41;
      int binIndex=gr41->GetXaxis()->FindBin(b41);
       gr41->GetXaxis()->SetBinLabel(binIndex,itr.data());
       gr41->GetXaxis()->ChangeLabel(binIndex,60,-1,39,-1,-1);
@@ -440,7 +516,7 @@ TFile *file = TFile::Open(fpath.c_str());
   gr43->GetXaxis()->Set(1+cycle_ncl.size(),gr43->GetXaxis()->GetXmin(),1+cycle_ncl.size());
 
   for (auto itr : runs_ncl){
-     b43++;
+     ++b43;
      int binIndex=gr43->GetXaxis()->FindBin(b43);
       gr43->GetXaxis()->SetBinLabel(binIndex,itr.data());
       gr43->GetXaxis()->ChangeLabel(binIndex,60,-1,39,-1,-1);
@@ -458,12 +534,11 @@ TFile *file = TFile::Open(fpath.c_str());
   gr42->SetMarkerColor(kBlue);
   gr42->SetLineColor(kBlue);
   gr42->Draw("PL SAME"); 
-  auto legend1=new TLegend(0.809353,0.830621,0.951639,0.914941);
-  legend1->SetFillColor(0);
-  //legend1->SetHeader("Phi range","");
-  legend1->AddEntry(gr42,"leftmost bin filled","lp");
-  legend1->AddEntry(gr43,"rightmost bin filled","lp");
-  legend1->Draw();
+  auto legend2=new TLegend(0.809353,0.830621,0.951639,0.914941);
+  legend2->SetFillColor(0);
+  legend2->AddEntry(gr42,"leftmost bin filled","lp");
+  legend2->AddEntry(gr43,"rightmost bin filled","lp");
+  legend2->Draw();
  
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -479,7 +554,7 @@ TFile *file = TFile::Open(fpath.c_str());
   gr5->GetXaxis()->Set(1+cycle_occ.size(),gr5->GetXaxis()->GetXmin(),1+cycle_occ.size());
 
   for (auto itr : runs_occ){
-     b5++;
+     ++b5;
      int binIndex=gr5->GetXaxis()->FindBin(b5);
       gr5->GetXaxis()->SetBinLabel(binIndex,itr.data());
       gr5->GetXaxis()->ChangeLabel(binIndex,60,-1,39,-1,-1);
@@ -493,15 +568,26 @@ TFile *file = TFile::Open(fpath.c_str());
   
 TFile *f = new TFile("../Plots/TrackTask_Result_trends.root", "RECREATE");
 f->cd();
-c1->Write();
-c2->Write();
-c3->Write();
-c31->Write();
-c4->Write();
-c41->Write();
-c43->Write();
-c5->Write();
+c1->Write(); 
+c2->Write(); 
+c21->Write(); 
+c3->Write(); 
+c31->Write(); 
+c4->Write(); 
+c41->Write(); 
+c43->Write(); 
+c5->Write(); 
 f->Close();
+
+delete gr; delete c1;
+delete gr2; delete c2;
+delete gr21; delete gr22; delete legend; delete c21;
+delete gr3; delete c3;
+delete gr31; delete gr32; delete legend1; delete c31;
+delete gr4; delete c4;
+delete gr41; delete c41;
+delete gr42; delete gr43; delete legend2; delete c43;
+delete gr5; delete c5;
 
 }
 
